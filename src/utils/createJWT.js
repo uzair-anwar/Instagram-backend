505edf6645b8be30f5f8d4ebbfbb6d0231f0c71a
uzair-anwar <uzair.anwar@devsinc.com>
2022-09-15 13:43:00 +0500

    Delete profile feature files

--- src/utils/createJWT.js
+++
@@ -1,8 +0,0 @@
-const jwt = require("jsonwebtoken");
-const duration = "30d";
-
-exports.createJWT = (user) => {
-  return jwt.sign({ id: user.id }, process.env.SECRET, {
-    expiresIn: duration,
-  });
-};
